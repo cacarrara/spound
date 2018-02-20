f9a94a6939816fab4c44ccde189c2cbe41dd73e4
Caio Carrara <eu@caiocarrara.com.br>
2018-02-19 22:41:34 -0300

    Adds home and signin initial view

--- spound/core/urls.py
+++ spound/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from core import views
+
+
 urlpatterns = [
+    path('home/', views.home_view, name='home'),
+    path('signin/', views.signin_view, name='signin'),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
--- spound/core/views.py
+++ spound/core/views.py
@@ -1,1 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView
+
+from core.services import SpotifyAuthService, SpotifyService
+
+
+class SignInView(TemplateView):
+    template_name = 'signin.html'
+
+    def get(self, request, *args, **kwargs):
+        self.auth_service = SpotifyAuthService()
+        self.code = self.auth_service.parse_response_code(request.get_full_path())
+        if self.code:
+            return redirect(reverse('core:home'))
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        if not self.code:
+            context['auth_url'] = self.auth_service.get_authorize_url()
+        return context
+
+
+signin_view = SignInView.as_view()
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        auth_service = SpotifyAuthService()
+        access_token = auth_service.get_access_token()
+
+        if not access_token:
+            return redirect(reverse('core:signin'))
+
+        spotify_service = SpotifyService(access_token)
+        self.user = spotify_service.current_user()
+        self.playlists = spotify_service.current_user_playlists()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['user'] = self.user,
+        context['playlists'] = self.playlists['items'],
+
+        return context
+
+
+home_view = HomeView.as_view()
